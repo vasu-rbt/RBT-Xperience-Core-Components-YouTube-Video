326939b14e0746024cd040b6c36d6d236179a31d
vasu.yerramsetti <vasu.yerramsetti@raybiztech.com>
2022-08-25 11:15:43 +0530

    Initial commit

---
+++ CMSRegistration.cs
@@ -0,0 +1,11 @@
+﻿using CMS;
+using Kentico.PageBuilder.Web.Mvc;
+using RBT.Xperience.Core.Components;
+
+[assembly: AssemblyDiscoverable]
+[assembly: RegisterWidget("RBT.Xperience.Core.Components.YouTubeVideo",
+    typeof(YouTubeVideoWidgetViewComponent),
+    "YouTube Video",
+    typeof(YouTubeVideoWidgetProperties),
+    Description = "Insert the video from specified YouTube URL location..",
+    IconClass = "icon-brand-youtube")]
---
+++ Components/Widgets/YouTubeVideoWidget/YouTubeVideoWidgetViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Kentico.PageBuilder.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace RBT.Xperience.Core.Components
+{
+    public class YouTubeVideoWidgetViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ComponentViewModel<YouTubeVideoWidgetProperties> componentViewModel)
+        {
+            var properties = componentViewModel.Properties;
+            var viewModel = new YouTubeVideoWidgetViewModel
+            {
+                VideoURL = (properties.VideoURL != null) ? properties.VideoURL.Replace("/watch?v=", "/embed/") : string.Empty,
+                Width = properties.Width,
+                Height = properties.Height,
+                VideoParameters = GetYouTubeVideo(properties.IsShowRelatedVideos, properties.IsFullScreen, properties.IsAutoPlay)
+            };
+            return View("~/Components/Widgets/YouTubeVideoWidget/_YouTubeVideo.cshtml", viewModel);
+        }
+        private string GetYouTubeVideo(bool relVideos, bool fullScreen, bool autoPlay)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(autoPlay ? "&autoplay=1&mute=1" : string.Empty);
+            stringBuilder.Append(fullScreen ? "&fs=1" : "&fs=0");
+            stringBuilder.Append((!relVideos) ? "&rel=0" : string.Empty);
+            stringBuilder.Append("&enablejsapi=1&version=3");
+            return stringBuilder.ToString();
+        }
+
+    }
+}
